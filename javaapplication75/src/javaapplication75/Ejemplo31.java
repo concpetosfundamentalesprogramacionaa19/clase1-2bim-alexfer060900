b8a87ada705abf1585d04f4c8a9a371372626ef5
alexfer060900 <alexaguirrerojas@hotmail.com>
2019-05-30 11:33:19 -0500

    Trabajos con arreglos unidimensionales en Java

--- javaapplication75/src/javaapplication75/Ejemplo31.java
+++ javaapplication75/src/javaapplication75/Ejemplo31.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 public class Ejemplo31 {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Declaramos e inicializamos las variables y los arreglos 
         boolean bandera = true;
         double nota;
         int opcion;
@@ -23,49 +23,50 @@ public class Ejemplo31 {
         int[] datos = new int[6];
         String[] rangos = {"0-8", "8.1-12", "12.1-16", "16.1-19", "19.1-20",
                             "20.1-∞"};
+        // Se presenta un ciclo while que permitira que el usuario decida 
+        // cuando termina de ingresar datos 
         while (bandera == true){
-         System.out.println("Ingrese la nota del estudiante por favor.");
-        nota = entrada.nextDouble();
-        contador = contador + 1;
-        if(nota >= 0 && nota <=8){
+            System.out.println("Ingrese la nota del estudiante por favor.");
+            nota = entrada.nextDouble();
+            contador = contador + 1;
+            //Condicionales if que deciden en que posicion del arreglo aumentar
+            if(nota >= 0 && nota <=8){
             datos[0] = datos[0] + 1;
-        }else{
-            if(nota >= 8.1 && nota <= 12){
-                datos[1] = datos[1] + 1;
             }else{
-                if (nota >= 12.1 && nota <= 16){
-                  datos[2] = datos[2] + 1;  
+                if(nota >= 8.1 && nota <= 12){
+                datos[1] = datos[1] + 1;
                 }else{
-                    if(nota >= 16.1 && nota <= 19){
-                        datos[3] = datos[3] + 1;
+                    if (nota >= 12.1 && nota <= 16){
+                        datos[2] = datos[2] + 1;  
                     }else{
-                        if (nota >= 19.1 && nota <= 20){
-                            datos[4] = datos[4] + 1;
+                        if(nota >= 16.1 && nota <= 19){
+                            datos[3] = datos[3] + 1;
                         }else{
-                            datos[5] = datos[5] + 1;
+                            if (nota >= 19.1 && nota <= 20){
+                                datos[4] = datos[4] + 1;
+                                }else{
+                                    datos[5] = datos[5] + 1;
+                                }
                         }
                     }
                 }
-                
             }
-        }
+            //Se le pregunta al usuario si desea ingresar mas datos
             System.out.println("Presione 1 si desea ingresar mas notas, o 2 "
                     + "para salir.");
             opcion = entrada.nextInt();
-            if (opcion == 1){
-                bandera = true;
-            }else{
-                bandera = false;
-            }
+                if (opcion == 1){
+                    bandera = true;
+                    }else{
+                        bandera = false;
+                    }
         }
         
-        
+        //Impresion final con el numero de notas ingresadas, asi como los
+        //rangos con el numero de veces que se repiten
         System.out.printf("Notas ingresadas: %d\nRangos:\n", contador);
         for(int i = 0; i<datos.length; i++){
             System.out.printf("%s = %d\n", rangos[i], datos[i]);
         }
-        
-
-
     }
 }
